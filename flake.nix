aa25ca103285b525e6fbae079593b6bef60d8501
zokrezyl <zokrezyl@gmail.com>
2025-07-04 12:39:49 +0200

    ni

--- flake.nix
+++ flake.nix
@@ -1,28 +1,10 @@
 {
-  description = "Clang + libc++ + CMake dev shell";
+  description = "Project using shared clang dev shell from GitHub";
 
-  #inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
-  outputs = { self, nixpkgs }: {
-    devShells = {
-      x86_64-linux = let
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
-        };
-        llvm = pkgs.llvmPackages;
-      in {
-        default = llvm.libcxxStdenv.mkDerivation {
-          name = "clang-libcxx-shell";
-          buildInputs = [
-            llvm.clang
-            llvm.lld
-            pkgs.cmake
-            pkgs.python314
-          ];
-        };
-      };
-    };
+  outputs = { self, clang-flake }: {
+    nixpkgs = clang-flake.nixpkgs;
+    devShells.x86_64-linux.default = clang-flake.devShells.x86_64-linux.default;
   };
 }
 
