987b1a1396f1f4b3f8ced525708423710ee214e9
zokrezyl <zokrezyl@gmail.com>
2025-07-12 15:48:31 +0200

    ni

--- include/ycetl/array.hpp
+++ include/ycetl/array.hpp
@@ -1,12 +1,30 @@
 #pragma once
-#include <ycetl/trivial_array.hpp>
+#include <ycetl/cstddef.hpp>
 
 namespace ycetl {
 
-template <typename T, size_t N> class array {
-  ycetl::trivial_array<T, N> data_;
+// simple std::array like container
+template <typename T, ycetl::size_t N> struct array {
+  T buffer[N];
+  constexpr array() : buffer() {};
 
-public:
+  constexpr T *memory() noexcept { return static_cast<T *>(buffer); }
+  // constexpr _array(): bufer {};
+  constexpr T *data() noexcept { return buffer; }
+  constexpr const T *data() const noexcept { return buffer; }
+  constexpr ycetl::size_t size() const noexcept { return N; }
+
+  constexpr T &operator[](ycetl::size_t i) noexcept { return buffer[i]; }
+  constexpr const T &operator[](ycetl::size_t i) const noexcept {
+    return buffer[i];
+  }
+
+  /*
+  constexpr T *begin() noexcept { return buffer; }
+  constexpr T *end() noexcept { return buffer + N; }
+  constexpr const T *begin() const noexcept { return buffer; }
+  constexpr const T *end() const noexcept { return buffer + N; }
+  */
 };
 
 } // namespace ycetl
