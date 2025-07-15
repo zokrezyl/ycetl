261035b3f5dbb9782432257dc8de25af4b7fbb11
zokrezyl <zokrezyl@gmail.com>
2025-07-15 20:13:33 +0200

    preparing ground for allocator degrading mechanism

---
+++ unit_test/types/test_multitype_memory.cpp
@@ -0,0 +1,46 @@
+#include <boost/ut.hpp>
+#include <ycetl/memory.hpp>
+#include <ycetl/trivial_shared_ptr.hpp>
+#include <ycetl/type_system.hpp>
+
+using namespace boost::ut;
+using namespace ycetl;
+using namespace ycetl::memory;
+
+suite multitype_memory_suite = [] {
+  "multitype_memory_allocate"_test = [] {
+    constexpr auto test = [] {
+      using types = type_set<int, double>;
+      multitype_memory<dynamic_memory, types> mem;
+
+      int *int_ptr = mem.allocate<int>(10);
+      double *double_ptr = mem.allocate<double>(5);
+
+      bool result = (int_ptr != nullptr) && (double_ptr != nullptr);
+
+      mem.deallocate(int_ptr);
+      mem.deallocate(double_ptr);
+
+      return result;
+    };
+
+    expect(test());
+  };
+
+  "multitype_memory_shared_handlers"_test = [] {
+    constexpr auto test = [] {
+      using original_types = type_set<int, double>;
+      using downgraded_types = type_set<int>;
+
+      multitype_memory<dynamic_memory, original_types> original_mem;
+
+      auto downgraded_mem = multitype_memory<dynamic_memory, downgraded_types>(
+          original_mem.handler_type::template downgrade<apply_wrapper_t<
+              trivial_shared_ptr,
+              apply_wrapper_t<dynamic_memory, downgraded_types>>>());
+    };
+    expect(test());
+  };
+};
+
+int main(int, char **) { return 0; }
