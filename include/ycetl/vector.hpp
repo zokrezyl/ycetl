b8a3c08ceb3eae2b1d98647cc4c4f6cdc43d3c66
zokrezyl <zokrezyl@gmail.com>
2025-07-17 18:33:31 +0200

    refactoring vector

--- include/ycetl/vector.hpp
+++ include/ycetl/vector.hpp
@@ -13,7 +13,6 @@
 namespace ycetl {
 
 template <typename T, typename Memory, typename BackendMode> class vector;
-;
 
 template <typename T,
           typename Memory =
@@ -23,32 +22,50 @@ class vector;
 
 /* detect nested vector --------------------------------------------------- */
 template <class> struct is_vector : std::false_type {};
-template <class U, class A> struct is_vector<vector<U, A>> : std::true_type {};
+template <class U, class A, class BM>
+struct is_vector<vector<U, A, BM>> : std::true_type {};
 
-template <typename T, typename Memory, bool Const> class vector_iterator {
+template <typename T, typename Memory, bool Const, typename BackendMode> // Added BackendMode
+class vector_iterator {
 public:
   using iterator_category = std::random_access_iterator_tag;
   using storage_unit = backend_type_of_t<T>;
-  using raw = std::conditional_t<Const, const storage_unit *, storage_unit *>;
+  // raw should be the underlying pointer type, not the storage_unit* itself
+  using raw_ptr = std::conditional_t<Const, const storage_unit *, storage_unit *>;
 
   using value_type = T;
   using difference_type = std::ptrdiff_t;
-  using pointer = value_type *;
-  using reference = value_type;
+  using pointer = std::conditional_t<Const, const value_type *, value_type *>;
+  using reference = std::conditional_t<Const, const value_type &, value_type &>;
 
 private:
-  raw _ptr = nullptr;
-  Memory _memory;
+  raw_ptr _ptr = nullptr;
+  // Using [[no_unique_address]] for the memory to optimize for empty memory types
+  [[no_unique_address]] Memory _memory;
 
 public:
   constexpr vector_iterator() = default;
-  constexpr vector_iterator(raw p, Memory mem) : _ptr(p), _memory(mem) {}
+  constexpr vector_iterator(raw_ptr p, Memory mem) : _ptr(p), _memory(mem) {}
+
+  // Conversion constructor from non-const iterator to const iterator
+  template <bool OtherConst, typename = std::enable_if_t<Const && !OtherConst>>
+  constexpr vector_iterator(const vector_iterator<T, Memory, OtherConst, BackendMode> &other) // Added BackendMode
+      : _ptr(other._ptr), _memory(other._memory) {}
+
+  // Constructor from backend's basic_iterator
+  template <bool OtherConst>
+  constexpr vector_iterator(typename ycetl::dynamic_array<storage_unit>::basic_iterator<OtherConst> backend_it, Memory mem)
+      : _ptr(&*backend_it), _memory(mem) {}
+
 
   constexpr reference operator*() const {
     if constexpr (is_vector<T>::value)
-      return value_type(_memory, const_cast<storage_unit &>(*_ptr));
+      // For nested vectors, return a view_type (which typically takes the backend and memory)
+      // Assuming view_type constructor from backend and memory is available
+      return value_type(const_cast<storage_unit &>(*_ptr), _memory);
     else
-      return *_ptr;
+      // For non-nested types, return a reference to the underlying element
+      return reinterpret_cast<reference>(*_ptr);
   }
 
   // Minimal operations for random-access iterator:
@@ -104,17 +121,20 @@ public:
   friend constexpr auto operator<=>(vector_iterator l, vector_iterator r) {
     return l._ptr <=> r._ptr;
   }
+
+  // Allow access to _ptr and _memory for vector's begin/end
+  friend class vector<T, Memory, BackendMode>; // This line is now correct
 };
 
-/*â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€ vector â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€*/
+/*ÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐ vector ÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐÐ*/
 // clang-format off
-template <typename T, 
+template <typename T,
   typename Memory,
   typename BackendMode>
 class vector {
   // clang-format on
 public:
-  using traits = container::container_traits<vector, T, Memory>;
+  using traits = container::container_traits<vector, T, Memory, BackendMode>;
 
   using backend_type = typename traits::backend_type;
   using storage_unit = typename traits::storage_unit;
@@ -130,11 +150,11 @@ public:
   using size_type = typename traits::size_type;
   using difference_type = typename traits::difference_type;
 
-  using iterator = vector_iterator<value_type, memory_type, false>;
-  using const_iterator = vector_iterator<value_type, memory_type, true>;
+  using iterator = vector_iterator<value_type, memory_type, false, BackendMode>;
+  using const_iterator = vector_iterator<value_type, memory_type, true, BackendMode>;
 
 private:
-  memory_type _memory;
+  [[no_unique_address]] memory_type _memory; // Use [[no_unique_address]]
   backend_type _backend;
 
 public:
@@ -147,8 +167,9 @@ public:
   template <class, class, class> friend class vector;
 
   /* constructors (unchanged bodies, but _backend calls stay correct) */
-  constexpr vector() : _memory(), _backend() {}
-  explicit constexpr vector(memory_type mem) : _memory(mem), _backend() {}
+  // Added default constructor
+  constexpr vector() : _memory(), _backend(_memory) {}
+  explicit constexpr vector(memory_type mem) : _memory(mem), _backend(mem) {}
 
   constexpr vector(size_type n, const T &v)
       : _memory(), _backend(_memory, n, v) {}
@@ -176,18 +197,26 @@ public:
   constexpr vector(const vector &o)
       : _memory(o._memory), _backend(_memory, o._backend) {}
 
+  // This constructor needs to copy from the other backend, not the backend itself
   constexpr vector(const vector &o, memory_type mem)
       : _memory(mem), _backend(_memory, *o._backend) {}
 
   constexpr vector(vector &&o) noexcept
       : _memory(std::move(o._memory)), _backend(std::move(o._backend)) {}
 
-  constexpr vector(vector &&o, memory_type mem) : _memory(mem), _backend() {
-    reserve(o.size());
-    for (auto &e : *o._backend)
-      _backend.push_back(_memory, std::move(e));
-    o.clear();
+  constexpr vector(vector &&o, memory_type mem) : _memory(mem), _backend(mem) { // Initialize backend with mem
+    // No need to reserve and push_back, you can move the backend itself
+    // Or if moving elements, use the move constructor of backend_type
+    if (this->_memory == o._memory) { // If allocators are equal, just transfer ownership
+        _backend = std::move(o._backend);
+    } else { // Else, copy elements
+        reserve(o.size());
+        for (auto &e : o) // Iterate over 'o' using its iterators
+          push_back(std::move(e));
+        o.clear();
+    }
   }
+
 
   constexpr ~vector() = default;
 
@@ -218,33 +247,31 @@ public:
   /* modifiers ---------------------------------------------------------- */
   constexpr void push_back(const T &v) {
     if constexpr (is_vector<T>::value)
-      // the first _memory is for the vector itself, the second for the
-      // back-end
-      _backend.emplace_back(_memory, _memory, *v._backend); // copy backâ€‘end
+      // The inner vector should be copied using its backend
+      _backend.emplace_back(_memory, *v._backend);
     else
       _backend.push_back(_memory, v);
   }
   constexpr void push_back(T &&v) {
     if constexpr (is_vector<T>::value)
-      _backend.emplace_back(_memory, std::move(*v._backend)); // move backâ€‘end
+      _backend.emplace_back(_memory, std::move(*v._backend)); // move back-end
     else
       _backend.push_back(_memory, std::move(v));
   }
 
-  /*
-  template <class... Args> constexpr T &emplace_back(Args &&...args) {
-    return *_backend.emplace_back(_memory, std::forward<Args>(args)...);
-  }
-  */
   template <class... Args> constexpr auto emplace_back(Args &&...args) {
     if constexpr (is_vector<T>::value) {
-      auto &inner_ref =
-          *_backend.emplace_back(_memory, std::forward<Args>(args)...);
-      return T(inner_ref, _memory);
+      // Create an inner vector with the given memory and arguments
+      // Then emplace its backend into the outer vector's backend
+      // This assumes T can be constructed with Args and then its _backend accessed
+      T temp_inner_vec(_memory, std::forward<Args>(args)...); // Construct inner vector
+      auto& emplaced_backend_ref = *_backend.emplace_back(_memory, std::move(*temp_inner_vec._backend));
+      return T(emplaced_backend_ref, _memory); // Return a view/wrapper of the emplaced backend
     } else {
       return (*_backend.emplace_back(_memory, std::forward<Args>(args)...));
     }
   }
+
 
   constexpr iterator insert(iterator pos, const T &val) {
     size_type idx = pos - begin();
@@ -258,13 +285,17 @@ public:
   constexpr void pop_back() { _backend.pop_back(); }
 
   /* iterators ---------------------------------------------------------- */
-  constexpr iterator begin() noexcept { return {_backend.begin(), _memory}; }
-  constexpr iterator end() noexcept { return {_backend.end(), _memory}; }
+  // Ensure that begin()/end() for the vector itself return vector_iterator
+  // and that the underlying _backend.begin()/_backend.end() return raw pointers
+  // or iterators compatible with vector_iterator's constructor.
+  // Assuming _backend.begin()/_backend.end() give raw pointers (storage_unit*).
+  constexpr iterator begin() noexcept { return {_backend.data(), _memory}; }
+  constexpr iterator end() noexcept { return {_backend.data() + _backend.size(), _memory}; }
   constexpr const_iterator begin() const noexcept {
-    return {_backend.begin(), _memory};
+    return {_backend.data(), _memory};
   }
   constexpr const_iterator end() const noexcept {
-    return {_backend.end(), _memory};
+    return {_backend.data() + _backend.size(), _memory};
   }
 };
 
--- unit_test/containers/test_dynamic_array_1.cpp
+++ unit_test/containers/test_dynamic_array_1.cpp
@@ -1,360 +1,127 @@
 #include <boost/ut.hpp>
-
 #include <ycetl/dynamic_array.hpp>
-#include <ycetl/memory.hpp> // Assuming this contains your corrected memory.hpp content
+#include <ycetl/memory.hpp>
 #include <ycetl/types.hpp>
-
-#include <new> // For std::construct_at, std::destroy_at
-#include <numeric>
-#include <utility> // For std::exchange
 
 using namespace boost::ut;
 namespace mem = ycetl::memory;
 using ycetl::type_set;
 
-// Use multitype_dynamic_memory as the test allocator type, as instructed.
 template <class T>
 using test_allocator = ycetl::multitype_dynamic_memory<type_set<T>>;
 
-// A non-trivial type to test construction/destruction
 struct NonTrivial {
   int id;
   constexpr NonTrivial(int i = 0) : id(i) {}
-  ~NonTrivial() = default; // Non-constexpr destructor allowed at runtime
-  NonTrivial(const NonTrivial &other) = default;
-  NonTrivial &operator=(const NonTrivial &other) = default;
-  NonTrivial(NonTrivial &&other) noexcept : id(std::exchange(other.id, -1)) {}
-  NonTrivial &operator=(NonTrivial &&other) noexcept {
-    id = std::exchange(other.id, -1);
-    return *this;
-  }
   constexpr bool operator==(const NonTrivial &other) const {
     return id == other.id;
   }
 };
 
-// Main test suite for dynamic_array
 suite dynamic_array_suite = [] {
-  // These allocators will now be multitype_dynamic_memory instances
-  test_allocator<int> int_allocator{};
-  test_allocator<double> double_allocator{};
-  test_allocator<NonTrivial> nontrivial_allocator{};
-
-  // --- Tests for dynamic_array operations using multitype_memory ---
-
-  "default_construction"_test = [&] {
-    constexpr auto test_lambda = [&] { // Inner lambda for the test logic
-      ycetl::dynamic_array<int> arr{};
+  "default_construction"_test = [] {
+    constexpr auto test_lambda = [] {
+      ycetl::dynamic_array<int> arr;
       return arr.size() == 0_u && arr.capacity() == 0_u;
     };
-    // Use static_assert for truly constexpr tests, expect for runtime tests.
-    // This test, if arr() is constexpr, could be static_assert.
-    // If it requires runtime behavior due to allocator, use expect.
-    expect(test_lambda()); // Use expect for runtime checks
+    static_assert(test_lambda());
+    expect(test_lambda());
   };
 
-  "size_constructor"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr(int_allocator, 5);
-      bool ok = (arr.size() == 5_u && arr.capacity() == 5_u);
-      ok = ok && (arr.data() != nullptr);
-      for (std::size_t i = 0; i < arr.size(); ++i) {
-        ok = ok && (arr[i] == 0_i); // Check default-initialized values
-      }
-      return ok;
-    };
-    expect(test_lambda()); // Use expect due to dynamic allocation
-  };
-
-  "size_value_constructor"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr(int_allocator, 3, 42);
-      bool ok = (arr.size() == 3_u && arr.capacity() == 3_u);
-      ok = ok && (arr.data() != nullptr);
-      for (std::size_t i = 0; i < arr.size(); ++i) {
-        ok = ok && (arr[i] == 42_i);
-      }
-      arr.clear_and_deallocate_buffer(int_allocator);
+  "size_constructor"_test = [] {
+    auto test_lambda = [] {
+      test_allocator<int> alloc{};
+      ycetl::dynamic_array<int> arr(alloc, 5);
+      bool ok = arr.size() == 5_u && arr.capacity() == 5_u;
+      for (auto v : arr)
+        ok = ok && v == 0;
+      arr.clear_and_deallocate_buffer(alloc);
       return ok;
     };
     expect(test_lambda());
   };
 
-  "range_constructor"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      int initial_data[] = {1, 2, 3, 4};
-      ycetl::dynamic_array<int> arr(int_allocator, std::begin(initial_data), 4);
-      bool ok = (arr.size() == 4_u && arr.capacity() == 4_u);
-      ok = ok && (arr[0] == 1_i);
-      ok = ok && (arr[3] == 4_i);
-      arr.clear_and_deallocate_buffer(int_allocator);
+  "initializer_list_constructor"_test = [] {
+    auto test_lambda = [] {
+      test_allocator<int> alloc{};
+      ycetl::dynamic_array<int> arr(alloc, {10, 20, 30});
+      bool ok = arr.size() == 3_u && arr[0] == 10 && arr[2] == 30;
+      arr.clear_and_deallocate_buffer(alloc);
       return ok;
     };
     expect(test_lambda());
   };
 
-  "initializer_list_constructor"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr(int_allocator, {10, 20, 30});
-      bool ok = (arr.size() == 3_u && arr.capacity() == 3_u);
-      ok = ok && (arr[0] == 10_i);
-      ok = ok && (arr[2] == 30_i);
-      arr.clear_and_deallocate_buffer(int_allocator);
+  "push_back_nontrivial"_test = [] {
+    auto test_lambda = [] {
+      test_allocator<NonTrivial> alloc{};
+      ycetl::dynamic_array<NonTrivial> arr(alloc);
+      arr.push_back(alloc, NonTrivial(1));
+      arr.push_back(alloc, NonTrivial(2));
+      bool ok = arr.size() == 2_u && arr[0].id == 1 && arr[1].id == 2;
+      arr.clear_and_deallocate_buffer(alloc);
       return ok;
     };
     expect(test_lambda());
   };
 
-  "copy_move_constructor"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr1(int_allocator, {100, 200, 300});
-      ycetl::dynamic_array<int> arr2(std::move(arr1)); // Move construction
-
-      bool ok = (arr2.size() == 3_u && arr2.capacity() == 3_u);
-      ok = ok && (arr2[0] == 100_i);
-      ok = ok && (arr2[2] == 300_i);
-
-      // Original array should be in a valid, but unspecified state (typically
-      // empty/null)
-      ok = ok && (arr1.begin() == nullptr);
-      ok = ok && (arr1.size() == 0_u);
-      ok = ok && (arr1.capacity() == 0_u);
-
-      arr2.clear_and_deallocate_buffer(int_allocator);
+  "insert_middle"_test = [] {
+    auto test_lambda = [] {
+      test_allocator<int> alloc{};
+      ycetl::dynamic_array<int> arr(alloc, {1, 2, 4});
+      arr.insert(alloc, arr.begin() + 2, 3);
+      bool ok = arr.size() == 4_u && arr[2] == 3;
+      arr.clear_and_deallocate_buffer(alloc);
       return ok;
     };
     expect(test_lambda());
   };
 
-  "push_back_int"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr(int_allocator);
-      arr.push_back(int_allocator, 10);
-      bool ok = (arr.size() == 1_u && arr[0] == 10_i);
-      arr.push_back(int_allocator, 20);
-      ok = ok && (arr.size() == 2_u && arr[1] == 20_i);
-      arr.push_back(int_allocator, 30);
-      ok = ok && (arr.size() == 3_u && arr[2] == 30_i);
-      ok = ok && (arr.capacity() >= 3_u);
-      arr.clear_and_deallocate_buffer(int_allocator);
+  "resize_grow"_test = [] {
+    auto test_lambda = [] {
+      test_allocator<int> alloc{};
+      ycetl::dynamic_array<int> arr(alloc, {1, 2});
+      arr.resize(alloc, 5);
+      bool ok = arr.size() == 5_u && arr[2] == 0 && arr[4] == 0;
+      arr.clear_and_deallocate_buffer(alloc);
       return ok;
     };
     expect(test_lambda());
   };
 
-  "push_back_nontrivial"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<NonTrivial> arr(nontrivial_allocator);
-      arr.push_back(nontrivial_allocator, NonTrivial(1));
-      bool ok = (arr.size() == 1_u && arr[0].id == 1_i);
-      arr.push_back(nontrivial_allocator, NonTrivial(2));
-      ok = ok && (arr.size() == 2_u && arr[1].id == 2_i);
-      arr.clear_and_deallocate_buffer(nontrivial_allocator);
-      return ok;
+  "clear_and_deallocate_buffer"_test = [] {
+    auto test_lambda = [] {
+      test_allocator<int> alloc{};
+      ycetl::dynamic_array<int> arr(alloc, {1, 2, 3});
+      arr.clear_and_deallocate_buffer(alloc);
+      return arr.size() == 0_u && arr.capacity() == 0_u &&
+             arr.begin() == nullptr;
     };
     expect(test_lambda());
   };
 
-  "emplace_back"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<NonTrivial> arr(nontrivial_allocator);
-      NonTrivial *p = arr.emplace_back(nontrivial_allocator, 50);
-      bool ok = (arr.size() == 1_u && arr[0].id == 50_i);
-      ok = ok && (p == arr.data()); // Pointer returned should be to the element
-      arr.clear_and_deallocate_buffer(nontrivial_allocator);
-      return ok;
-    };
-    expect(test_lambda());
-  };
-
-  "pop_back"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr(int_allocator, {1, 2, 3});
-      arr.pop_back();
-      bool ok = (arr.size() == 2_u && arr[1] == 2_i);
-      arr.pop_back();
-      ok = ok && (arr.size() == 1_u && arr[0] == 1_i);
-      arr.pop_back();
-      ok = ok && (arr.size() == 0_u);
-      arr.clear_and_deallocate_buffer(int_allocator);
-      return ok;
-    };
-    expect(test_lambda());
-  };
-
-  "reserve_grow"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr(int_allocator);
-      bool ok = (arr.capacity() == 0_u);
-      arr.reserve(int_allocator, 10);
-      ok = ok && (arr.capacity() == 10_u);
-      ok = ok && (arr.size() == 0_u);
-      arr.clear_and_deallocate_buffer(int_allocator);
-      return ok;
-    };
-    expect(test_lambda());
-  };
-
-  "reserve_no_grow"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr(int_allocator, 5); // Capacity 5
-      bool ok = (arr.capacity() == 5_u);
-      arr.reserve(int_allocator, 3);      // Request smaller capacity
-      ok = ok && (arr.capacity() == 5_u); // Should not shrink
-      arr.clear_and_deallocate_buffer(int_allocator);
-      return ok;
-    };
-    expect(test_lambda());
-  };
-
-  "resize_shrink"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr(int_allocator, {1, 2, 3, 4, 5});
-      arr.resize(int_allocator, 3);
-      bool ok = (arr.size() == 3_u);
-      ok = ok && (arr[2] == 3_i);
-      arr.clear_and_deallocate_buffer(int_allocator);
-      return ok;
-    };
-    expect(test_lambda());
-  };
-
-  "resize_grow"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr(int_allocator, {1, 2});
-      arr.resize(int_allocator, 5);
-      bool ok = (arr.size() == 5_u);
-      ok = ok && (arr.capacity() >= 5_u);
-      ok = ok && (arr[0] == 1_i);
-      ok = ok && (arr[1] == 2_i);
-      ok = ok && (arr[2] == 0_i); // Default constructed
-      ok = ok && (arr[4] == 0_i); // Default constructed
-      arr.clear_and_deallocate_buffer(int_allocator);
-      return ok;
-    };
-    expect(test_lambda());
-  };
-
-  "clear"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr(int_allocator, {1, 2, 3});
-      arr.clear();
-      bool ok = (arr.size() == 0_u);
-      ok = ok &&
-           (arr.capacity() >= 3_u); // Capacity usually retained after clear
-      ok = ok && (arr.data() != nullptr); // Buffer is still there
-      arr.clear_and_deallocate_buffer(
-          int_allocator); // Deallocate for test cleanup
-      return ok;
-    };
-    expect(test_lambda());
-  };
-
-  "clear_and_deallocate_buffer"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr(int_allocator, {1, 2, 3});
-      arr.clear_and_deallocate_buffer(int_allocator);
-      bool ok = (arr.size() == 0_u);
-      ok = ok && (arr.capacity() == 0_u);
-      ok = ok && (arr.data() == nullptr);
-      return ok;
-    };
-    expect(test_lambda());
-  };
-
-  "insert_middle"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr(int_allocator, {1, 2, 4});
-      arr.insert(int_allocator, arr.begin() + 2, 3); // Insert 3 at index 2
-      bool ok = (arr.size() == 4_u);
-      ok = ok && (arr[0] == 1_i);
-      ok = ok && (arr[1] == 2_i);
-      ok = ok && (arr[2] == 3_i);
-      ok = ok && (arr[3] == 4_i);
-      arr.clear_and_deallocate_buffer(int_allocator);
-      return ok;
-    };
-    expect(test_lambda());
-  };
-
-  "insert_begin"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr(int_allocator, {2, 3});
-      arr.insert(int_allocator, arr.begin(), 1); // Insert 1 at beginning
-      bool ok = (arr.size() == 3_u);
-      ok = ok && (arr[0] == 1_i);
-      ok = ok && (arr[1] == 2_i);
-      ok = ok && (arr[2] == 3_i);
-      arr.clear_and_deallocate_buffer(int_allocator);
-      return ok;
-    };
-    expect(test_lambda());
-  };
-
-  "insert_end"_test = [&] {
-    constexpr auto test_lambda = [&] {
-      ycetl::dynamic_array<int> arr(int_allocator, {1, 2});
-      arr.insert(int_allocator, arr.end(), 3); // Insert 3 at end
-      bool ok = (arr.size() == 3_u);
-      ok = ok && (arr[0] == 1_i);
-      ok = ok && (arr[1] == 2_i);
-      ok = ok && (arr[2] == 3_i);
-      arr.clear_and_deallocate_buffer(int_allocator);
-      return ok;
-    };
-    expect(test_lambda());
-  };
-
-  // Your specific test example, adjusted for multitype_memory and proper
-  // Boost.UT syntax
-  "FUCKING_TEST_EXAMPLE_FOR_GOOGLE_IDIIOT"_test = [&] {
-    constexpr auto test_lambda = [&] {
+  "multitype_memory_usage"_test = [] {
+    auto test_lambda = [] {
       using memory_t =
-          mem::multitype_memory<mem::dynamic_memory, type_set<int>>;
-      memory_t memory; // This allocates an internal dynamic_memory instance
+          mem::multitype_memory<mem::dynamic_memory, type_set<int, double>>;
+      memory_t memory;
 
-      ycetl::dynamic_array<int> arr(memory,
-                                    4); // Uses the multitype_memory instance
-      for (int i = 0; i < 4; ++i) {
-        arr[i] = i + 1;
-      }
-
-      int sum = 0;
-      for (int i = 0; i < 4; ++i)
-        sum += arr[i];
-
-      // Cleanup: Must deallocate the buffer used by dynamic_array
-      arr.clear_and_deallocate_buffer(
-          memory); // Pass the same memory instance for deallocation
-
-      return sum == 10;
-    };
-    // This test involves dynamic allocation (from dynamic_memory), so
-    // static_assert will likely fail. static_assert(test_lambda()); //
-    // Uncomment if your dynamic_memory is fully constexpr
-    expect(test_lambda()); // Use expect for runtime execution
-  };
-
-  // Another example of direct multitype_memory usage, similar to your previous
-  // "multitype_alloc_int_double"_test
-  "multitype_direct_usage"_test = [] {
-    constexpr auto test_lambda = [] {
-      using types = type_set<int, double>;
-      mem::multitype_memory<mem::dynamic_memory, types> st;
-
-      int *pi = st.template allocate<int>(2);
-      double *pd = st.template allocate<double>(1);
+      int *pi = memory.allocate<int>(2);
+      double *pd = memory.allocate<double>(1);
 
       pi[0] = 7;
       pi[1] = 11;
       pd[0] = 2.0;
 
-      bool ok = (pi[0] + pi[1] == 18 && pd[0] == 2.0_d);
+      bool ok = pi[0] + pi[1] == 18 && pd[0] == 2.0;
 
-      st.template deallocate<int>(pi, 2);    // Deallocate with size
-      st.template deallocate<double>(pd, 1); // Deallocate with size
+      memory.deallocate<int>(pi);
+      memory.deallocate<double>(pd);
+
       return ok;
     };
-    expect(test_lambda()); // Use expect due to dynamic allocation
+    expect(test_lambda());
   };
 };
+
+int main() {}
